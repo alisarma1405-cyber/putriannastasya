5c142a0a70a8604443b6b99b9222c30cde9eeba2
alisarma1405-cyber <alisarma1405@gmail.com>
2026-09-15 13:26:12 +0700

    Add files via upload

---
+++ data-tuple.py
@@ -0,0 +1,10 @@
+# tuple with int as element's data type
+tuple_1=(2,4,6)
+
+# tuple with str as element's data type 
+tuple_2 = ("numenor", "valinor")
+
+# tuple with various data type in the element 
+tuple_3 = (14, True, "Ahn Keonho")
+
+print(tuple_3[2])
---
+++ operator-assign.py
@@ -0,0 +1,10 @@
+# deklarasi variabel num_1
+num_1 = 14
+
+# deklarasi variabel num_2
+num_1 = 18
+
+# deklarasi variabel num_3
+num_1 = 25
+
+# deklarasi variabel num_3 dengan isi nilai hasil operasi aritmatika num_1 + num_2 
